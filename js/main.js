501f91099ee2f342368efe4dd96f02f555677766
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-04-01 21:27:59 +0530

    uh still broken I just tried fixing fonts

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    window.scrollTo(0, 1);
-});
+
 window.addEventListener("scroll", (event) => {
     let scrollY = this.scrollY;
     let scrollX = this.scrollX;
+    console.log(scrollY)
     if(scrollY > 30) {
         document.getElementById("nav").style.padding = "21px 50px";
         document.getElementById("nav").style.backgroundColor = "rgba(0, 0, 0, 0.510)";
-        document.getElementById("nav").style.backdropFilter = "blur(20px)";
+        document.getElementById("nav").style.backdropFilter = "blur(12px)";
         document.getElementById("nav").style.transition = "all 0.3s";
     } else {
         document.getElementById("nav").style.padding = "33px 50px";
@@ -17,6 +16,11 @@ window.addEventListener("scroll", (event) => {
     }
 });
 
-//in view
-
-
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
