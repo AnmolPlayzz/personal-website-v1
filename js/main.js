3416cbbd222ab4e875f44dd648ef1993ec2ee1ce
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-04-20 12:01:05 +0530

    Contact page improvements and Animations
    1. Contact page has been re-done.
    2. Scroll animations on the home page have been introduced.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 console.log("da nub site™")
+window.addEventListener('DOMContentLoaded', (event) => {
+  window.scrollTo(0, 0);  
+});
+
+
 window.addEventListener("scroll", (event) => {
     let scrollY = this.scrollY;
     let scrollX = this.scrollX;
@@ -12,6 +17,43 @@ window.addEventListener("scroll", (event) => {
         document.getElementById("nav").style.backgroundColor = "rgba(47, 37, 72, 0)";
         document.getElementById("nav").style.backdropFilter = "blur(0px)";
         document.getElementById("nav").style.transition = "all 0.3s";
+    }
+
+    var isInViewport = function (elem) {
+      var bounding = elem.getBoundingClientRect();
+      return (
+          bounding.top >= 0 &&
+          bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      );
+    };
+    var tit1 = document.getElementById('c1');
+    var tit2 = document.getElementById('vidh');
+    var tit3 = document.getElementById('vid');
+    var tit4 = document.getElementById('footer2');
+    if (isInViewport(tit1)) {
+      document.getElementById("c1").style.opacity = "1";
+      document.getElementById("c1").style.transform = "translateY(0px)";
+      document.getElementById("c1").style.transition = "all 0.3s 0.1s ease";
+      document.getElementById("c2").style.opacity = "1";
+      document.getElementById("c2").style.transform = "translateY(0px)";
+      document.getElementById("c2").style.transition = "all 0.3s 0.3s ease";
+    }
+    if (isInViewport(tit2)) {
+      document.getElementById("vidh").style.opacity = "1";
+      document.getElementById("vidh").style.transform = "scale(1)";
+      document.getElementById("vidh").style.transition = "all 0.3s 0.2s ease";
+    }
+    if (isInViewport(tit3)) {
+      document.getElementById("vid").style.opacity = "1";
+      document.getElementById("vid").style.transform = "scale(1) translateY(0px)";
+      document.getElementById("vid").style.transition = "all 0.3s 0s ease";
+    }
+    if (isInViewport(tit4)) {
+      document.getElementById("footer1").style.opacity = "1";
+      document.getElementById("footer2").style.opacity = "1";
+      document.getElementById("footer2").style.transform = " translateY(0px)";
+      document.getElementById("footer1").style.transition = "all 0.3s 0s ease";
+      document.getElementById("footer2").style.transition = "all 0.6s 0.3s ease";
     }
 });
 
