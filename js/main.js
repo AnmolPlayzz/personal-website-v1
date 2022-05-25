25fec69f158495bea2d24cfaaa31aa2ed5c615ff
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-05-25 11:16:26 +0530

    #5 and #4 Fixed

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ window.addEventListener("scroll", (event) => {
     let scrollY = this.scrollY;
     let scrollX = this.scrollX;
     if(scrollY > 30) {
-        document.getElementById("nav").style.padding = "21px 50px";
+        document.getElementById("nav").style.padding = "21px 0px";
         document.getElementById("nav").style.backgroundColor = "rgba(0, 0, 0, 0.510)";
         document.getElementById("nav").style.backdropFilter = "blur(12px)";
         document.getElementById("nav").style.transition = "all 0.3s";
     } else {
-        document.getElementById("nav").style.padding = "33px 50px";
+        document.getElementById("nav").style.padding = "33px 0px";
         document.getElementById("nav").style.backgroundColor = "rgba(47, 37, 72, 0)";
         document.getElementById("nav").style.backdropFilter = "blur(0px)";
         document.getElementById("nav").style.transition = "all 0.3s";
