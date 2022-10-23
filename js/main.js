3624f09a5b41cdca396045a299d81ddfbcccc632
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-10-23 22:42:52 +0530

    Fix: too quick transition

--- js/main.js
+++ js/main.js
@@ -130,16 +130,16 @@ var animation = bodymovin.loadAnimation({
 
 function tran() {
     document.querySelector('body').style.opacity = "0"
-    document.querySelector('body').style.transition = "all 0.3s ease";
-    setTimeout(() => { document.location.href="/" }, 300);
+    document.querySelector('body').style.transition = "all 0.4s ease";
+    setTimeout(() => { document.location.href="/" }, 700);
 };
 function tran1() {
     document.querySelector('body').style.opacity = "0"
-    document.querySelector('body').style.transition = "all 0.3s ease";
-    setTimeout(() => { document.location.href="/socials" }, 300);
+    document.querySelector('body').style.transition = "all 0.4s ease";
+    setTimeout(() => { document.location.href="/socials" }, 700);
 };
 function tran2() {
     document.querySelector('body').style.opacity = "0"
-    document.querySelector('body').style.transition = "all 0.3s ease";
-    setTimeout(() => { document.location.href="/contact" }, 300);
+    document.querySelector('body').style.transition = "all 0.4s ease";
+    setTimeout(() => { document.location.href="/contact" }, 700);
 };
