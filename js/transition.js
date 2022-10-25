57ec7bf2d07413713c4d0249f544188668ba1c6a
AnmolPlayzz <anmol@anmolplayzz.tk>
2022-10-25 17:27:49 +0000

    Commit

--- js/transition.js
+++ js/transition.js
@@ -3,6 +3,7 @@ document.addEventListener('readystatechange', event => {
 
     if (event.target.readyState === "complete") {
       setTimeout(() => {
+          document.querySelector('body').style.transform = "translateY(0%)"
           document.querySelector('body').style.opacity = "1"
           document.querySelector('body').style.transition = "all 0.6s ease";
        }, 500);
@@ -10,17 +11,21 @@ document.addEventListener('readystatechange', event => {
   });
 
 function tran() {
+
     document.querySelector('body').style.opacity = "0"
-    document.querySelector('body').style.transition = "all 0.4s ease";
+    document.querySelector('body').style.transform = "translateY(-40%)"
+    document.querySelector('body').style.transition = "all 0.5s ease";
     setTimeout(() => { document.location.href="/" }, 700);
 };
 function tran1() {
     document.querySelector('body').style.opacity = "0"
     document.querySelector('body').style.transition = "all 0.4s ease";
+    document.querySelector('body').style.transform = "translateY(-40%)"
     setTimeout(() => { document.location.href="/socials" }, 700);
 };
 function tran2() {
     document.querySelector('body').style.opacity = "0"
     document.querySelector('body').style.transition = "all 0.4s ease";
-    setTimeout(() => { document.location.href="/contact" }, 700);
+    document.querySelector('body').style.transform = "translateY(-40%)"
+    setTimeout(() => { document.location.href="/contact" }, 600);
 };
