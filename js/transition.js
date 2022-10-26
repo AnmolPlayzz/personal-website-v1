8a99e135d5c6ac24d99b57a3582dfe2017fe618c
AnmolPlayzz <anmol@anmolplayzz.tk>
2022-10-26 08:09:43 +0000

    Smoother / Less laggy transitions.

--- js/transition.js
+++ js/transition.js
@@ -3,7 +3,6 @@ document.addEventListener('readystatechange', event => {
 
     if (event.target.readyState === "complete") {
       setTimeout(() => {
-          document.querySelector('body').style.transform = "translateY(0%)"
           document.querySelector('body').style.opacity = "1"
           document.querySelector('body').style.transition = "all 0.6s ease";
        }, 500);
@@ -13,19 +12,16 @@ document.addEventListener('readystatechange', event => {
 function tran() {
 
     document.querySelector('body').style.opacity = "0"
-    document.querySelector('body').style.transform = "translateY(-40%)"
     document.querySelector('body').style.transition = "all 0.5s ease";
     setTimeout(() => { document.location.href="/" }, 700);
 };
 function tran1() {
     document.querySelector('body').style.opacity = "0"
     document.querySelector('body').style.transition = "all 0.4s ease";
-    document.querySelector('body').style.transform = "translateY(-40%)"
     setTimeout(() => { document.location.href="/socials" }, 700);
 };
 function tran2() {
     document.querySelector('body').style.opacity = "0"
     document.querySelector('body').style.transition = "all 0.4s ease";
-    document.querySelector('body').style.transform = "translateY(-40%)"
     setTimeout(() => { document.location.href="/contact" }, 600);
 };
